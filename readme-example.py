e20d8038029694eb59d515a00551d825bba19c28
Will Dean <wd60622@gmail.com>
2025-06-23 21:15:56 -0400

    adding some additional markdown

--- readme-example.py
+++ readme-example.py
@@ -21,6 +21,28 @@ def _(mo):
     # PyMC x Marimo
 
     This notebook is based on the linear regression example in the README of PyMC repo. Find the original example [here](https://github.com/pymc-devs/pymc?tab=readme-ov-file#linear-regression-example).
+
+    There are independent variables:
+
+    - Sunlight Hours: Number of hours the plant is exposed to sunlight daily.
+    - Water Amount: Daily water amount given to the plant (in milliliters).
+    - Soil Nitrogen Content: Percentage of nitrogen content in the soil.
+
+    as well as the dependent variable:
+
+    - Plant Growth (y): Measured as the increase in plant height (in centimeters) over a certain period.
+
+    The functional form of the model is:
+
+    $$
+    y_i \sim \alpha + \mathbf{X}\beta + \epsilon_i
+    $$
+
+    with
+
+    $$
+    \epsilon_i \sim \mathcal{N}(0, \sigma^2)
+    $$
     """
     )
     return
@@ -30,9 +52,15 @@ def _(mo):
 def _():
     import marimo as mo
 
+    import arviz as az
     import pymc as pm
 
-    return mo, pm
+    import matplotlib.pyplot as plt
+
+    az.style.use("arviz-darkgrid")
+    plt.rcParams["figure.figsize"] = [12, 7]
+    plt.rcParams["figure.dpi"] = 100
+    return az, mo, pm
 
 
 @app.cell(hide_code=True)
@@ -41,18 +69,11 @@ def _(mo):
         r"""
     ## Sampling from PyMC distributions
 
-    Taking draws from a normal distribution
+    We will generate $\mathbf{X}$ by taking draws from a normal distribution.
 
-    Independent Variables:
+    PyMC distribution outside of modelcontext can be created with the `dist` classmethod. This returns a `TensorVariable` which is from the `pytensor` package.
 
-    - Sunlight Hours: Number of hours the plant is exposed to sunlight daily.
-    - Water Amount: Daily water amount given to the plant (in milliliters).
-    - Soil Nitrogen Content: Percentage of nitrogen content in the soil.
-
-
-    Dependent Variable:
-
-    - Plant Growth (y): Measured as the increase in plant height (in centimeters) over a certain period.
+    The `pm.draw` function can be used to generate samples from any `TensorVariable`.
     """
     )
     return
@@ -60,7 +81,7 @@ def _(mo):
 
 @app.cell
 def _(pm):
-    seed = 42
+    seed = sum(map(ord, "PyMC x Marimo"))
     x_dist = pm.Normal.dist(shape=(100, 3))
     x_data = pm.draw(x_dist, random_seed=seed)
 
@@ -120,11 +141,53 @@ def _(mo):
 
 
 @app.cell
-def _(generative_model, pm):
+def _(mo):
+    def beta_slider(value: float):
+        return mo.ui.slider(start=-30, value=value, stop=30, show_value=True, step=0.01)
+
+    fixed_betas = mo.ui.dictionary(
+        {
+            "sunlight hours": beta_slider(5),
+            "water amount": beta_slider(20),
+            "soil nitrogen": beta_slider(2),
+        }
+    )
+
+    fixed_sigma = mo.ui.slider(
+        start=0.01, value=0.5, stop=5, show_value=True, step=0.01
+    )
+
+    mo.vstack(
+        [
+            fixed_betas,
+            fixed_sigma,
+        ]
+    )
+    return fixed_betas, fixed_sigma
+
+
+@app.cell
+def _(mo):
+    run_following_cells = mo.ui.button(label="Run Following Cells")
+
+    run_following_cells
+    return (run_following_cells,)
+
+
+@app.cell
+def _(run_following_cells):
+    run_following_cells.value
+    return
+
+
+@app.cell
+def _(fixed_betas, fixed_sigma, generative_model, mo, pm, run_following_cells):
+    mo.stop(not run_following_cells.value)
+
     # Generating data from model by fixing parameters
     fixed_parameters = {
-        "betas": [5, 20, 2],
-        "sigma": 0.5,
+        "betas": [*fixed_betas.value.values()],
+        "sigma": fixed_sigma.value,
     }
     synthetic_model = pm.do(generative_model, fixed_parameters)
 
@@ -141,11 +204,20 @@ def _(mo):
 @app.cell
 def _(pm, seed, synthetic_model):
     with synthetic_model:
-        prior_idata = pm.sample_prior_predictive(random_seed=seed)
+        prior_idata = pm.sample_prior_predictive(
+            random_seed=seed, compile_kwargs={"mode": "NUMBA"}
+        )
         synthetic_y = prior_idata.prior["plant_growth"].sel(draw=0, chain=0)
 
     synthetic_y
     return (synthetic_y,)
+
+
+@app.cell
+def _(az, synthetic_y):
+    ax = az.plot_dist(synthetic_y)
+    ax.set(title="Marginal distribution of plant_growth")
+    return
 
 
 @app.cell
@@ -175,7 +247,7 @@ def _(mo):
         r"""
     ## Bayesian Inference
 
-    Use `pm.sample` .
+    Use `pm.sample` to sample the unknown parameters of the model using NUTS. By specifying the `nuts_sampler="nutpie"`, we can use NUTS sampler written in Rust.
     """
     )
     return
@@ -183,15 +255,14 @@ def _(mo):
 
 @app.cell
 def _(inference_model, pm, seed):
-    # Infer parameters conditioned on observed data
     with inference_model:
         idata = pm.sample(random_seed=seed, nuts_sampler="nutpie")
     return (idata,)
 
 
 @app.cell
-def _(idata, pm):
-    summary = pm.stats.summary(idata, var_names=["betas", "sigma"])
+def _(az, idata):
+    summary = az.summary(idata, var_names=["betas", "sigma"])
 
     summary
     return
@@ -203,7 +274,9 @@ def _(mo):
         r"""
     ## Out of sample predictions
 
-    The inference model can
+    The `inference_model` includes the `x` with shape `(100, 3)`. However, we can use `pm.set_data` to change the `x` to a new value.
+
+    After updating `x`, we can see both `x` and observed `plant_growth` have changed in the graph.
     """
     )
     return
@@ -213,10 +286,10 @@ def _(mo):
 def _(coords, inference_model, pm, seed):
     # Simulate new data conditioned on inferred parameters
     new_x_data = pm.draw(
-        pm.Normal.dist(shape=(3, 3)),
+        pm.Normal.dist(shape=(4, 3)),
         random_seed=seed,
     )
-    new_coords = coords | {"trial": [0, 1, 2]}
+    new_coords = coords | {"trial": [0, 1, 2, 3]}
 
     with inference_model:
         pm.set_data({"x": new_x_data}, coords=new_coords)
@@ -241,16 +314,22 @@ def _(idata, inference_model, pm, seed):
 
 @app.cell
 def _(mo):
-    mo.md(r"""## Counterfactual""")
+    mo.md(
+        r"""
+    ## Counterfactual
+
+    The `pm.do` operator can be used again to simulate new data under a scenario where the first beta is zero
+    """
+    )
     return
 
 
 @app.cell
 def _(inference_model, pm):
-    # Simulate new data, under a scenario where the first beta is zero
+    mask = [0, 1, 1]
     plant_growth_model = pm.do(
         inference_model,
-        {inference_model["betas"]: inference_model["betas"] * [0, 1, 1]},
+        {inference_model["betas"]: inference_model["betas"] * mask},
     )
 
     plant_growth_model
